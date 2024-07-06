51f0c8359504a0a1d9e5651c48c46fd3521dcaf7
Priyanka-25-testing <pppatil0717@gmail.com>
2024-07-06 20:50:10 +0530

    Update AlternateArrayElements.java

--- src/mentorship/AlternateArrayElements.java
+++ src/mentorship/AlternateArrayElements.java
@@ -1,5 +1,5 @@
 package mentorship;
-//add comment
+//Print even and odd number in the given array
 public class AlternateArrayElements {
 	public void print(int x[]) {
 		for (int i = 0; i < x.length; i++) {
