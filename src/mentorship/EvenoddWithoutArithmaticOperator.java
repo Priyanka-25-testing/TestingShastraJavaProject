331e89e7ea306dee0f5db9000fa2b3419ff13041
Priyanka-25-testing <pppatil0717@gmail.com>
2024-08-07 17:21:17 +0530

    first commit

--- src/mentorship/EvenoddWithoutArithmaticOperator.java
+++ src/mentorship/EvenoddWithoutArithmaticOperator.java
@@ -7,6 +7,12 @@ public class EvenoddWithoutArithmaticOperator {
 		System.out.println("Enter the number:");
 		Scanner sc= new Scanner(System.in);
 		int num=sc.nextInt();
+		evenoddnumUsingConditions(num);
+		System.out.println(evenOddNumberWithoutConditions(num)?"Even" : "Odd");
+		
+	}
+		private static void evenoddnumUsingConditions(int num) {
+		
 		if((num & 1)==0) {
 			System.out.println("Number "+num+" is Even");
 		}
@@ -14,5 +20,16 @@ public class EvenoddWithoutArithmaticOperator {
 			System.out.println("Number  "+num+" is odd");
 		}
 	}
+		public static boolean evenOddNumberWithoutConditions(int num){
+			return (num & 1)==0;
+		}
 
 }
+// 10 = 2*3  2*2 2*1 2*0
+//        8    4   2   1
+//  10       1    0    1   0
+//    9     1    0     0   1
+//    1     0   0     0    1
+//  10 & 1= 0    0    0    0  for even
+//  
+//  9&1   = 0001 for odd
